bceb2cc72db7dbc2236ab4a7551b07a7779e0e35
Cipherpy <reshmababuraj89@gmail.com>
2017-04-15 11:52:39 +0530

    Update asosaccessories.py

--- asosaccessories.py
+++ asosaccessories.py
@@ -1,13 +1,23 @@
-import scrapy
+import re
 
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ["asos.com"]
-    start_urls = ("http://www.asos.com/women/accessories/cat/?cid=4174&pgesize=36")
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from asos_women.items import AsosWomenItem
 
-    #rules = (
-        #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    #)
+class AsosSpider(CrawlSpider):
+    name = 'asos'
+    allowed_domains = ['http://www.asos.com/women/accessories/cat/?cid=4174']
+    start_urls = ['http://www.http://www.asos.com/women/accessories/cat/?cid=4174/']
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = AsosWomenItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
