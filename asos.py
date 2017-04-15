e4be3298e397aab324b9403a509dbcd250607f0f
Cipherpy <reshmababuraj89@gmail.com>
2017-04-16 04:33:10 +0530

    error occured-
    ubuntu 12.04
    python 2.7
    scrapy 0. some version
    error: Exception error:the HtmlXPathSelector doesnt have the attribute XPath
    
    Expected Solution:
    Try to upgrade to ubuntu 14
    try to install python 3.4

--- asos.py
+++ asos.py
@@ -1,13 +1,30 @@
-from scrapy.spider import BaseSpider
+import re
+
 from scrapy.selector import HtmlXPathSelector
-from asos_women.items import AsosWomenItem
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from asoswomen.items import AsoswomenItem
 
-class AsosSpider(BaseSpider):
+class MySpider(CrawlSpider):
     name = 'asos'
-    allowed_domains = ['http://www.asos.com/women/accessories/cat/?cid=4174']
-    start_urls = ['http://www.asos.com/women/accessories/cat/?cid=4174']
+    allowed_domains = ['www.asos.com']
+    start_urls = ['http://www.asos.com/women/accessories/cat/?cid=4174&pgesize=36']
 
-    
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//li[@class="next"]',)),callback='parse_item', follow=True),
+    )
 
-    def parse(self, response): # parse is the default callback method,if we change the name it wont work
-      name=response.xpath('h1').extract
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        titles = hxs.xpath("//li[@class='product-container interactions']")
+        #i = AsoswomenItem()
+        items = []
+        for title in titles:
+            item = AsoswomenItem()
+            item["name"] = title.select(".//span[@class='name']/text()").extract()
+            item["price"] = title.select(".//span[@class='price']/text()").extract()
+            items.append(item)
+        return items
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['descri
