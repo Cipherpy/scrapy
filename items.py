703439edaf7532409888a9033e4d815570b140f5
Cipherpy <reshmababuraj89@gmail.com>
2017-04-16 13:12:52 +0530

    Update items.py
    added the item image

--- items.py
+++ items.py
@@ -1,11 +1,17 @@
+# -*- coding: utf-8 -*-
+
 # Define here the models for your scraped items
 #
 # See documentation in:
-# http://doc.scrapy.org/topics/items.html
+# http://doc.scrapy.org/en/latest/topics/items.html
+
 import scrapy
+
 from scrapy.item import Item,Field
 
-class AsosWomenItem(Item):
+class AsoswomenItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = Field()
-    pass
+     
+        price = Field()
+        name = Field()
+        image=Field()
