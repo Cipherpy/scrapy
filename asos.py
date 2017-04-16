f93e93f570d4a984efdae332978212fb735a5019
Cipherpy <reshmababuraj89@gmail.com>
2017-04-16 12:59:24 +0530

    Updated OS_Scrap
    Upgraded OS to Ubuntu 14.04, python 3.5 ,Scrapy 1.3.3
    Code for scraping a single page has been tested. 
    Scraping was successful

--- asos.py
+++ asos.py
@@ -1,30 +1,20 @@
-import re
-
+from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
 from asoswomen.items import AsoswomenItem
 
-class MySpider(CrawlSpider):
-    name = 'asos'
-    allowed_domains = ['www.asos.com']
-    start_urls = ['http://www.asos.com/women/accessories/cat/?cid=4174&pgesize=36']
+class MySpider(BaseSpider):
+    name = "asos"
+    allowed_domains = ["asos.com"]
+    start_urls = ["http://www.asos.com/women/accessories/cat/?cid=4174&pgesize=36"]
 
-    rules = (
-        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//li[@class="next"]',)),callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
+    def parse(self, response):
         hxs = HtmlXPathSelector(response)
         titles = hxs.xpath("//li[@class='product-container interactions']")
-        #i = AsoswomenItem()
         items = []
+        
         for title in titles:
             item = AsoswomenItem()
             item["name"] = title.select(".//span[@class='name']/text()").extract()
-            item["price"] = title.select(".//span[@class='price']/text()").extract()
+            item["price"] =title.select(".//span[@class='price']/text()").extract()
             items.append(item)
         return items
-        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
-        #i['name'] = hxs.select('//div[@id="name"]').extract()
-        #i['descri
