14475fe9fec4266fa54e41bea37cc6e7e4a0e61c
Cipherpy <reshmababuraj89@gmail.com>
2017-04-16 13:52:46 +0530

    Update asos.py
    Crawled multiple pages

--- asos.py
+++ asos.py
@@ -1,13 +1,20 @@
-from scrapy.spider import BaseSpider
+from scrapy.spider import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from asoswomen.items import AsoswomenItem
 
-class MySpider(BaseSpider):
+from scrapy.linkextractors import LinkExtractor
+
+from asoswomen.items import AsoswomenItem
+
+class MySpider(CrawlSpider):
     name = "asos"
     allowed_domains = ["asos.com"]
     start_urls = ["http://www.asos.com/women/accessories/cat/?cid=4174&pgesize=36"]
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//li[@class="next"]',)), callback="parse_items", follow= True),
+    )
 
-    def parse(self, response):
+    def parse_items(self, response):
         hxs = HtmlXPathSelector(response)
         titles = hxs.xpath("//li[@class='product-container interactions']")
         items = []
